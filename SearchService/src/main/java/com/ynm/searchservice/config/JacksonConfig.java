b8af12b2a8f720f1b86fbe7556e9ddeb1f336125
Yead <119938455+yeadkhail@users.noreply.github.com>
2025-08-30 14:44:16 +0600

    implemented security for searchservice and also added eureka service discovery.

---
+++ SearchService/src/main/java/com/ynm/searchservice/config/JacksonConfig.java
@@ -0,0 +1,30 @@
+package com.ynm.searchservice.config;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.ynm.searchservice.dto.UserDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.IOException;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public Module securityModule() {
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(UserDetails.class, new UserDetailsDeserializer());
+        return module;
+    }
+
+    static class UserDetailsDeserializer extends JsonDeserializer<UserDetails> {
+        @Override
+        public UserDetails deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            return ctxt.readValue(p, UserDto.class);
+        }
+    }
+}
