dedec7be3f20f459398cc22d5678248b7b141367
Nafi1548 <masuqalislamnafi@gmail.com>
2025-09-28 16:36:34 +0600

    Add sync-on-score feature and test controller

---
+++ ResearchPaperService/src/main/java/com/ynm/researchpaperservice/Controller/TestUserScoreController.java
@@ -0,0 +1,26 @@
+package com.ynm.researchpaperservice.controller;
+
+import com.ynm.researchpaperservice.service.UserScoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/test-sync")
+@RequiredArgsConstructor
+public class TestUserScoreController {
+
+    private final UserScoreService userScoreService;
+
+    @PostMapping("/user-score")
+    public void syncUserScore(
+            @RequestParam Integer userId,
+            @RequestParam Float newUpdate,
+            @RequestParam Float lastUpdate) {
+        userScoreService.syncScore(userId, newUpdate, lastUpdate);
+    }
+    @GetMapping("/get-by-email")
+    public Float getUserScoreByEmail(@RequestParam String email) {
+        System.out.println(userScoreService.getUserScoreByEmail(email));
+        return userScoreService.getUserScoreByEmail(email);
+    }
+}
