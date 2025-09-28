c09c7f733a22510b76654f900067611275bf06b0
Nafi1548 <masuqalislamnafi@gmail.com>
2025-09-28 16:33:46 +0600

    paper service now communicates with user service on score update and get user score

--- userManagementService/src/main/java/com/ynm/usermanagementservice/controller/UserController.java
+++ userManagementService/src/main/java/com/ynm/usermanagementservice/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.ynm.usermanagementservice.controller;
 
 import com.ynm.usermanagementservice.dto.UserDto;
+import com.ynm.usermanagementservice.dto.UserScoreSyncRequest;
+import com.ynm.usermanagementservice.model.User;
+import com.ynm.usermanagementservice.repository.UserRepository;
 import com.ynm.usermanagementservice.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/users")
@@ -11,6 +16,7 @@ public class UserController {
     private final UserService userService;
 
     public UserController(UserService userService) {
+
         this.userService = userService;
     }
 
@@ -24,5 +30,20 @@ public class UserController {
             System.out.println("User found with email: " + email);
         }
         return dto;
+    }
+    @PutMapping("/syncScore")
+    public void updateUserScore(@RequestBody UserScoreSyncRequest request) {
+        System.out.println(request.getLastUpdate());
+        System.out.println(request.getUserId());
+        System.out.println(request.getNewUpdate());
+    }
+
+    @GetMapping("/email/{email}/score")
+    public Float getUserScoreByEmail(@PathVariable String email) {
+        User user = userService.getUserByEmail(email);
+        if (user == null) {
+            throw new RuntimeException("User not found with email: " + email);
+        }
+        return user.getUserMetice();  // returns Float
     }
 }
